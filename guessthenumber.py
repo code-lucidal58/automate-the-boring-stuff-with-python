8bbbf88b6c1ec771b69b7a26e3bbba865c4c98d8
code-lucidal58 <aanisha.mishra05@gmail.com>
2019-06-25 09:01:24 +0530

    guess the number added

---
+++ guessthenumber.py
@@ -0,0 +1,21 @@
+# Guess the number game
+import random
+
+if __name__ == '__main__':
+    name = input("Hello! What is your name? ")
+    print("Well {}! I am thinking of a number between 1 and 20.".format(name))
+    print("You can make maximum 6 guesses")
+    x = random.randint(1, 20)
+    c = 0
+    for i in range(1, 7):
+        guess = int(input("Take a guess. "))
+        if guess == x:
+            print("Good! You guessed in {} guesses".format(c))
+            break
+        elif guess > x:
+            print("Guess is too high")
+        else:
+            print("Guess is too low")
+        c = i+1
+    if c == 6:
+        print("Nope. the number I was thinking is {}".format(x))
